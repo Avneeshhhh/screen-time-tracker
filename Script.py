acb1ee12ce55d8c7a129ca2a6b93b614848d4375
Avneesh Yadav <avneeshyadav0115@gmail.com>
2026-07-14 23:58:59 +0530

    Tracking logic

--- Script.py
+++ Script.py
@@ -27,3 +27,43 @@ cursor.execute("""
 conn.commit()
 
 tracking = False
+
+usage_data = {}
+session_seconds = 0
+
+def get_active_app():
+    try:
+        hwnd = win32gui.GetForegroundWindow()
+        _, pid = win32process.GetWindowThreadProcessId(hwnd)
+        process = psutil.Process(pid)
+        return process.name()
+    except:
+        return "Unknown"
+
+def tracking_loop():
+    global tracking 
+
+    while tracking:
+        app = get_active_app()
+        usage_data[app] = usage_data.get(app, 0) + 1
+        update_dashboard()
+        time.sleep(1)
+
+
+def save_to_database():
+    today = datetime.now().strftime("%Y-%m-%d")
+    for app, seconds in usage_data.items()
+    cursor.execute("""
+INSERT INTO usage(
+                   app_name,
+                   usage_seconds,
+                   date)
+                   Values(?,?,?)
+""", (app, seconds, today))
+    conn.commit()
+
+
+def update_dashboard():
+    pass
+
+print(get_active_app())
